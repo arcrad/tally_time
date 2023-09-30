60e0638f3743580b1478d8778a5df56ecebd909f
arcrad <jonathanmaiorana@gmail.com>
2023-09-30 00:40:54 -0400

    eslint + basic functionality
    adjusted eslint config for production build
    implemented basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+
+type TallySet = Date[];
 
 function App() {
   const [count, setCount] = useState(0)
+	const [tallySet, setTallySet] = useState<TallySet>( () => {
+		let tallySet:TallySet|null = localStorage.getItem("tallySet");
+		if(tallySet !== null) {
+		  return JSON.parse(tallySet).map( entry => new Date(entry) );
+		}
+		return [];
+	});
+
+	useEffect( () => {
+			localStorage.setItem("tallySet", JSON.stringify(tallySet));
+	}, [tallySet]);
+
+	function addTally() {
+		setTallySet( (cs) => {
+			return [...cs, new Date()];
+		});
+	}
+	
+	function resetTallySet() {
+		if(window.confirm('Are you sure? This will delete all current tallies.')) {
+			setTallySet([]);
+		}
+	}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Tally Time</h1>
+			<h2>{tallySet.length}</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={addTally}>
+          Add Tally
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+				<button onClick={resetTallySet}>
+					Reset
+				</button>
+				<div className="tallyListContainer">
+					{
+						tallySet.length === 0 ? 
+							<span>No tallys yet...</span>
+							:
+							<ul className="tallyList">
+								{ tallySet
+										.reverse()
+										.map( (ts, i) => <li key={i}>
+											#{tallySet.length-i} @ {ts.toLocaleString()}</li>
+										)
+								}
+							</ul>
+					}
+				</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
