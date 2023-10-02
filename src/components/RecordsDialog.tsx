ee35fbe0717305b23325b7ef64a814ca4295ce71
arcrad <jonathanmaiorana@gmail.com>
2023-10-02 01:06:16 -0400

    tweaked RecordsDialog component styles

--- src/components/RecordsDialog.tsx
+++ src/components/RecordsDialog.tsx
@@ -13,28 +13,49 @@ export type Ref = HTMLDialogElement;
 
 const RecordsDialog = forwardRef<Ref, RecordsDialogProps>((props, recordSelectDialogRef) => {
 	return (
-		<dialog ref={recordSelectDialogRef}>
-			<button onClick={ () => recordSelectDialogRef.current.close()}>
-				Close Modal
-			</button>
+		<dialog 
+			className="p-4 rounded-xl backdrop:bg-purple-900 backdrop:bg-opacity-80"
+			ref={recordSelectDialogRef}
+		>
+			<h1 className="text-sm uppercase font-bold px-2 py-2">Available Tally Records</h1>
 			<ul>
 				{
 					props.tallyRecords.map( (record, i) => {
 						return ( 
 							<li
+								className="text-lg leading-loose"
 								style={{
 									fontWeight: `${i == props.currentRecordIndex ? 'bold' : 'normal'}`
 								}}
 								key={i} 
-								onClick={() => { 
-									props.updateActiveRecord(i)
-								}}
-							>{record.title}</li> 
+							>
+								<button
+									className="w-full text-left rounded-md px-2 hover:bg-gray-200"
+									onClick={() => { 
+										props.updateActiveRecord(i)
+									}}
+								>
+									{record.title}
+								</button>
+							</li> 
 						)
 					})
 				}
 			</ul>
-			<button onClick={props.createNewRecord}>+++ New Record</button>
+			<div className="mt-2 space-x-2 flex flex-row justify-center items-center">
+			<button 
+				className="border border-green-500 text-lg px-2 py-1 rounded-md text-green-600 hover:bg-green-100"
+				onClick={props.createNewRecord}
+			>
+			 + New Record
+			</button>
+				<button 
+					className="border border-gray-600 text-lg px-2 py-1 rounded-md text-gray-700 hover:bg-gray-200"
+					onClick={ () => recordSelectDialogRef.current.close()}
+				>
+					Close
+				</button>
+			</div>
 		</dialog>
 	);
 });
