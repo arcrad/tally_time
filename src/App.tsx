7cb0177311dcb30b95e5eb19e9aa69ff730dbbd3
arcrad <jonathanmaiorana@gmail.com>
2023-10-04 22:08:40 -0400

    cleaned up vite autogen template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import "./App.css";
 import { TallyRecords } from "./commonTypes";
 import TallyList from "./components/TallyList";
 import RecordsDialog from "./components/RecordsDialog";
